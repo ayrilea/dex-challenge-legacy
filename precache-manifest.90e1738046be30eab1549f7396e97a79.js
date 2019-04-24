f1ae1e492f4ff6a703687cea69ffc9800e4e9798
Stefan Petrucev <stefan.petrucev@gmail.com>
2019-04-25 01:23:34 +1000

    Updates

---
+++ precache-manifest.90e1738046be30eab1549f7396e97a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8632f9108ab28aa4145a875a2a729488",
+    "url": "/dex-challenge/index.html"
+  },
+  {
+    "revision": "4be69bab843d89c2c58e",
+    "url": "/dex-challenge/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dex-challenge/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4be69bab843d89c2c58e",
+    "url": "/dex-challenge/static/js/main.87fb04e0.chunk.js"
+  },
+  {
+    "revision": "d8ab000de91f95884791",
+    "url": "/dex-challenge/static/js/runtime~main.9d0c4ade.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dex-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
