2aa425837b471d0df126485bacf759602ac66fe3
Stefan Petrucev <stefan.petrucev@gmail.com>
2019-04-25 22:06:34 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/dex-challenge/precache-manifest.90e1738046be30eab1549f7396e97a79.js"
+  "/dex-challenge/precache-manifest.6729031c2ba08d35e71986ea45854344.js"
 );
 
 self.addEventListener('message', (event) => {
