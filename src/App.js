56c8f2856b02dec32a2dd1b57aaa21fbc5ac9e89
Stefan Petrucev <stefan.petrucev@gmail.com>
2019-04-25 16:43:30 +1000

    Add DexChallenge
    DexChallenge includes:
    - number of Pokemon entered
    - number of Pokemon remaining
    - input field and submit button for entering Pokemon
    
    Input can be done via either clicking the button or submitting the form
    (e.g. pressing enter when the input is selected). Invalid input
    (duplicate or not present) causes the input styling to be invalid, and valid
    input causes the input styling to be valid.
    
    DexChallenge is mobile friendly and should work at all browser sizes.
    
    Currently there is no display of previously correct entries.
    
    Changes:
    - Removed uneeded create-react-app scaffolding
    - Added bootstrap and consumed it via ReactBootstrap
    - Added node-sass to consume bootstrap mixins (used for background
      colour on the body element)
    - Added prop-types and verified pokemon data prop type
    - Added script to call PokeAPI can get names of Kanto Pokemon, format
      them and write them to data file
    - Updated App component to load data and pass it to DexChallenge
      component
    - Added DexChallenge component with all game logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import DexChallenge from "./DexChallenge";
+import "./App.scss";
+import PokemonData from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return <DexChallenge pokemon={PokemonData} />;
+  }
 }
 
 export default App;
---
+++ src/DexChallenge.js
@@ -0,0 +1,135 @@
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import "./DexChallenge.scss";
+
+class DexChallenge extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      input: "",
+      pokemon: this.props.pokemon,
+      valid: null,
+      total: Object.keys(this.props.pokemon).length
+    };
+  }
+
+  /**
+   * Handle when entry is submitted and it is invalid.
+   *
+   * This method sets the invalid styling on the input field.
+   */
+  handleInvalidEntry = () => {
+    this.setState(state => ({
+      valid: false
+    }));
+  };
+
+  /**
+   * Handle when entry is submitted and it is valid.
+   *
+   * This method removes the entry from the remaining list, updates the counter, clears
+   * the input and sets the valid styling on the input field.
+   */
+  handleValidEntry = () => {
+    delete this.state.pokemon[this.state.input];
+
+    this.setState(state => ({
+      count: state.count + 1,
+      input: "",
+      valid: true
+    }));
+  };
+
+  /**
+   * @return true if the challenge is complete, otherwise false
+   */
+  isComplete = () => {
+    return this.state.total === this.state.count;
+  };
+
+  /**
+   * Returns whether or not the current input is valid.
+   *
+   * The current input is what is entered in the input field when submit is invoked.
+   * Valid input is when the input matches one of the items in the remaining list,
+   * case insensitive.
+   *
+   * @return true if the current input is valid, otherwise false
+   */
+  isInputValid = () => {
+    return this.state.pokemon.hasOwnProperty(this.state.input.toLowerCase());
+  };
+
+  /**
+   * When input changes, updates input state and clears valid styling.
+   */
+  onChange = ({ target }) => {
+    this.setState(state => ({
+      input: target.value,
+      valid: null
+    }));
+  };
+
+  /**
+   * When input is submit, refocuses input and applies handling depending on whether or
+   * not input is valid.
+   */
+  onSubmit = e => {
+    e.preventDefault();
+    this.inputNode.focus();
+    if (this.isInputValid()) {
+      this.handleValidEntry();
+    } else {
+      this.handleInvalidEntry();
+    }
+  };
+
+  render() {
+    const complete = this.isComplete();
+
+    return (
+      <div className="game-container p-3 text-center text-light">
+        <h1>{this.state.count}</h1>
+        <h4>Remaining: {this.state.total - this.state.count}</h4>
+        <div className="mt-4">
+          {complete && <h3>Complete!</h3>}
+          {!complete && (
+            <form onSubmit={this.onSubmit}>
+              <InputGroup className={"mt-4"}>
+                <FormControl
+                  aria-describedby="basic-addon2"
+                  aria-label="Enter a Pokemon"
+                  autoComplete="false"
+                  autoFocus={true}
+                  isInvalid={this.state.valid === false}
+                  isValid={this.state.valid}
+                  onChange={this.onChange}
+                  placeholder="Enter a Pokemon"
+                  ref={ref => (this.inputNode = ref)}
+                  spellCheck={false}
+                  value={this.state.input}
+                />
+                <InputGroup.Append>
+                  <Button onClick={this.onSubmit} variant="outline-light">
+                    Submit
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </form>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+DexChallenge.propTypes = {
+  pokemon: PropTypes.object.isRequired
+};
+
+export default DexChallenge;
